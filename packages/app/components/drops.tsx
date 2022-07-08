1ca7609f5513581a373b87986b1f32436820f06e
kyriediculous <vergauwennico@gmail.com>
2022-07-08 04:09:08 +0200

    app: remove unused imports

--- packages/app/components/drops.tsx
+++ packages/app/components/drops.tsx
@@ -1,10 +1,10 @@
-import { FC, useEffect, useState} from "react";
-import { Subgraph, Drop } from "@/utils/queries";
+import { FC } from "react";
+import { Drop } from "@/utils/queries";
 import { useAccount } from 'wagmi'
 
 export const Drops: FC<{drops: Array<Drop>, showClaimed?: boolean}> = ({drops, showClaimed}) => {
     const {data: account} = useAccount()
-
+    
     let dropsParsed = !showClaimed ? drops : drops.filter(drop => {
         for (let i=0; i < drop.claims.length; i++) {
             if (drop.claims[i].id.includes(account?.address || "")) return false;
@@ -17,8 +17,6 @@ export const Drops: FC<{drops: Array<Drop>, showClaimed?: boolean}> = ({drops, s
             {
                 dropsParsed
                 .map(drop => {
-                    console.log()
-                    console.log(drop.claims[0].id)
                     return (<div key={drop.id}>
                         {drop.id} {drop.uri}
                     </div>
